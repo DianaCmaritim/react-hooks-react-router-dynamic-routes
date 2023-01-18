0ac7470ab067a4b6d0bc05f44ae5dd961110e2dd
Diana <maritimdiane96@gmail.com>
2023-01-18 15:10:35 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
+//import MovieShow from "./MovieShow";
 
 function App() {
   const [movies, setMovies] = useState({
